e4b71087408b65f70c36c03c6d2c5aa653ab881c
Adel Gaznanov <a.gaznanov@innopolis.university>
2024-12-06 17:09:21 +0300

    feat: add Exponential non-linear transformation

---
+++ src/main/java/backend/academy/fractal/transformations/ExponentialTransformation.java
@@ -0,0 +1,15 @@
+package backend.academy.fractal.transformations;
+
+public class ExponentialTransformation implements Transformation {
+    private static final double SCALE = 1.5;
+
+    @Override
+    public double[] transform(double x, double y) {
+        double exp = Math.exp(x - 1);
+
+        double newX = SCALE * exp * Math.cos(Math.PI * y);
+        double newY = SCALE * exp * Math.sin(Math.PI * y);
+        return new double[] {newY, newX};
+    }
+
+}
