c8ef34d0e17e614809ea16146364faac960cbf40
Adel Gaznanov <a.gaznanov@innopolis.university>
2024-12-06 16:51:14 +0300

    feat: add Fisheye non-linear transformation

---
+++ src/main/java/backend/academy/fractal/transformations/FisheyeTransformation.java
@@ -0,0 +1,14 @@
+package backend.academy.fractal.transformations;
+
+public class FisheyeTransformation implements Transformation {
+    private static final double SCALE = 1;
+
+    @Override
+    public double[] transform(double x, double y) {
+        double r = Math.sqrt(x * x + y * y);
+        double factor = 2 / (r + 1);
+        double newX = SCALE * factor * x;
+        double newY = SCALE * factor * y;
+        return new double[] {newX, newY};
+    }
+}
