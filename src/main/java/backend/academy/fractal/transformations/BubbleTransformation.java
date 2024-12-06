6859274589f05640fa78f53ed53f0abf6bf17884
Adel Gaznanov <a.gaznanov@innopolis.university>
2024-12-06 16:11:57 +0300

    feat: add Bubble non-linear transformation

---
+++ src/main/java/backend/academy/fractal/transformations/BubbleTransformation.java
@@ -0,0 +1,15 @@
+package backend.academy.fractal.transformations;
+
+public class BubbleTransformation implements Transformation {
+    private static final double SCALE = 2;
+    private static final int BUBBLE_RADIUS = 4;
+
+    @Override
+    public double[] transform(double x, double y) {
+        double rSquared = x * x + y * y;
+        double factor = BUBBLE_RADIUS / (rSquared + BUBBLE_RADIUS);
+        double bubbleX = SCALE * factor * x;
+        double bubbleY = SCALE * factor * y;
+        return new double[] {bubbleX, bubbleY};
+    }
+}
